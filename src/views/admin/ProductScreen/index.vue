367a1f0b88784b883719033d54490072fe1e6909
HoangPhuc <phucndh@dn.paracelsoft.com>
2022-07-21 17:39:49 +0700

    update src

--- src/views/admin/ProductScreen/index.vue
+++ src/views/admin/ProductScreen/index.vue
@@ -2,10 +2,18 @@
 <template>
   <div class="hp-admin-product-main">
     <h1>{{ $t("product.product") }}</h1>
-    <hr>
+    <hr />
     <div>
-      
+      <b-form-input></b-form-input>
+      <b-form-input></b-form-input>
+      <b-form-input></b-form-input>
+      <b-form-input></b-form-input>
     </div>
+    <div>
+      <b-button variant="success">Button</b-button>
+      <b-button variant="outline-primary">Button</b-button>
+    </div>
+    <b-table striped hover></b-table>
   </div>
 </template>
 
